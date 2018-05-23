5d03950d917cb77e2f514ec373271fdbca1fc640
lcs-tdauletov <32198299+lcs-tdauletov@users.noreply.github.com>
2018-05-23 16:56:52 -0400

    Irate pirate is BAAAACK!!!!! No one can stop me. NO ONE.

--- PigLatin/main.swift
+++ PigLatin/main.swift
@@ -1,39 +1,87 @@
-//
-//  main.swift
-//  PigLatin
-//
-
 import Foundation
 
-// INPUT
-// Global variable, tracks how many words we can expect to have to translate to Pig Latin
-var countOfExpectedWordsToTranslate = 3
 
-// Write a loop to actually collect the expected count of words to be translated from user
-// e.g.: write the rest of the INPUT section
+// The variable that we are to work with
 
+var intNumberOfRealLines = 0
 
-// PROCESS & OUTPUT
-// Implement the primary logic of the problem here
-// Some output may be given here if you desire
+// Input sorting
 
-// Collect the words to be translated
-for counter in 1...countOfExpectedWordsToTranslate {
+while true {
+    print("How many lines do you want me to convert in Pig Latin?")
+    // Checking for the nill inputs
+    guard let givenNumberOfLines = readLine()  else {
+        continue
+        
+        
+    }
+    // Integer conversion
+    guard let intNumber = Int(givenNumberOfLines) else  {
+        
+        continue
+        
+    }
     
-    // Ask user for the word to be translated
-    print("Enter word #\(counter):")
     
-    // Create a variable that has the translated word
-    var translatedWord = ""
+    //Final check
     
-    // Get the input (use guard-let to guarantee it is not nil)
-    // and then print it out
-    guard let givenInput = readLine() else {
-        // If someone enters nil input, just skip to the next line
+    if intNumber < 0 || intNumber > 10 {
         continue
     }
     
-    // Replace this logic with the correct logic
-    translatedWord = givenInput
+    // All clear, let's roll
     
+    intNumberOfRealLines = intNumber
+    break
 }
+
+// Process
+
+//First of all, the declarations are crucial.
+
+var newStringArray : [Character] = []
+// Well, this might provoke people, but stil...
+var mediocreStringLikeTheLifeItself = ""
+for i in 1...intNumberOfRealLines {
+    print("Enter the word number \(i)")
+    
+    guard let givenEnglishWord = readLine() else {
+        continue
+    }
+    
+    var charArray : [Character] = Array(givenEnglishWord)
+    
+    for i in 0...charArray.count - 1 {
+        if charArray[i] == "A" || charArray[i] == "E" || charArray[i] == "I" || charArray[i] == "O" || charArray[i] == "U"  {
+            // For this stroke, the man went through hesitation.
+            for j in 0...charArray.count - 1 - i {
+                newStringArray.append(charArray[i+j])
+            }
+            break
+        } else {
+            
+            // See, not useless after all.
+            // The reason for the existance of this minion is that when charArray was tried with the
+            // newstringArray.append(charArray[i]) it would simply append it in the
+            // Gramatically correct order.
+            // I was quite baffled when my PARDON was turned into PARDONAY.
+            // Then I figured that I better decelerate this piece of code below so that it reaches my
+            // Forged string after the ARDON part.
+            // So this minion was conceived. Not a minion that you wanna brag in front of your friends about,
+            // But it's a solution.
+            mediocreStringLikeTheLifeItself += String(charArray[i])
+        }
+        }
+        
+    }
+
+
+var string = String(newStringArray)
+string += mediocreStringLikeTheLifeItself
+string += "AY"
+print("\(string)")
+
+
+
+
+
